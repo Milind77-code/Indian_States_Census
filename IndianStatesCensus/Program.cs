f21ff17b5bcbf250912a628b69a94e82627551c5
Milind_77 <milinddhote77@gmail.com>
2021-08-15 16:28:16 +0530

    [Milind] Add: Ability to load the analyser to load state census data from csv file.

---
+++ IndianStatesCensus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IndianStatesCensus
+{
+    class Program
+    {
+        /// <summary>
+        /// Indian State census problem statement..
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<--------India State Census Problem statement-------->");
+        }
+    }
+}
---
+++ IndianStatesCensus/StateCensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IndianStatesCensus
+{
+    class StateCensusAnalyser
+    {
+        /// <summary>
+        /// loads the data directly from the csv file
+        /// </summary>
+        public int GetData()
+        {
+            string filepath = @"C:\Users\Milind\OneDrive\Desktop\all Programs\Indian_States_Census\IndianStatesCensus\Indian state census data\IndiaStateCensusData.csv";
+            string[] data = File.ReadAllLines(filepath);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            return data.Length;
+        }
+    }
+}
